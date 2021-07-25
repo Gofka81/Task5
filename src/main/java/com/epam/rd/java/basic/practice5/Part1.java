564cb9407d273f21a18fd499e73e597e1ee80052
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-07-25 22:56:59 +0300

    finished part 1

--- src/main/java/com/epam/rd/java/basic/practice5/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part1.java
@@ -3,7 +3,45 @@ package com.epam.rd.java.basic.practice5;
 public class Part1 {
 
     public static void main(String[] args) {
+        Thread t = new MyThreadC();
+        Thread r = new Thread(new MyThreadI());
 
+        t.start();
+        try{
+            t.join(4000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        r.start();
     }
 
+}
+
+class MyThreadC extends Thread{
+    @Override
+    public void run() {
+        for(int i = 0; i<4; ++i){
+            System.out.println(getName());
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class MyThreadI implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i = 0; i<4; ++i){
+            System.out.println(Thread.currentThread().getName());
+            try{
+                Thread.sleep(500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
