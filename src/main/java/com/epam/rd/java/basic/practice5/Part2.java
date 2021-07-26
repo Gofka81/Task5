5fa0462306c88b2b3f9bd47a83119d0b48920bc8
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-07-26 17:40:11 +0300

    Part 2.1

--- src/main/java/com/epam/rd/java/basic/practice5/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part2.java
@@ -3,6 +3,7 @@ package com.epam.rd.java.basic.practice5;
 public class Part2 {
 
     public static void main(final String[] args) {
+        Spam spam = new Spam(new String[]{"Dat","why","how","4","1"}, new int[]{1000,333,2000,0,5000});
 
     }
 
--- src/main/java/com/epam/rd/java/basic/practice5/Spam.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Spam.java
@@ -1,27 +1,69 @@
 package com.epam.rd.java.basic.practice5;
 
+import java.util.Scanner;
+
 public class Spam {
 
     private Thread[] threads;
 
     public Spam(final String[] messages, final int[] delays) {
-
+        threads = new Thread[messages.length];
+        for(int i =0; i< threads.length; i++){
+            threads[i] = new Worker(messages[i], delays[i]);
+        }
     }
 
     public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String readedString = sc.nextLine();
+        while (readedString != null) {
+            if (readedString.isEmpty()) {
 
+                break;
+            }
+            if (sc.hasNextLine()) {
+                readedString = sc.nextLine();
+            } else {
+                readedString = null;
+            }
+        }
+        sc.close();
     }
 
     public void start() {
-
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
     public void stop() {
-
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static class Worker extends Thread {
+        private int delay;
+        private String message;
 
+        public Worker(String message, int delay){
+            this.message = message;
+            this.delay = delay;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(message);
+            try {
+                sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
