6fdbb092399f97dff84586c4f9befeede7de5425
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-07-29 01:15:45 +0300

    Part 3.5

--- src/main/java/com/epam/rd/java/basic/practice5/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part3.java
@@ -33,23 +33,7 @@ public class Part3 {
 
 
     public static void main(final String[] args) {
-        Part3 t = new Part3(10,3);
-        t.startAsync();
-        try {
-            for(Thread thread: t.myThreads){
-                thread.join();
-            }
-        } catch (InterruptedException e) {
-            return;
-        }
-        t.startSync();
-        try {
-            for(Thread thread: t.myThreads){
-                thread.join();
-            }
-        } catch (InterruptedException e) {
-            return;
-        }
+
     }
 
     public void compare() {
